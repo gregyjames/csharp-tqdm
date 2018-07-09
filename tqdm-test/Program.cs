f60c3947be9380170f322fe18cd1603f45393c71
Greg James <madra.uc@gmail.com>
2018-07-09 19:25:45 -0400

    Removed test message

---
+++ tqdm-test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace tqdm_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new tqdm();
+            x.set_theme_basic();
+            for(int i = 0; i < 100; i++)
+            {
+                x.progress(i, 100);
+                Thread.Sleep(50);
+            }
+            x.finish();
+            Console.ReadLine();
+        }
+    }
+}
